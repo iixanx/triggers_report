5bed502582d36bf0e0e8db97ecd9ec8397db7a77
iixanx <iixiixanx@gmail.com>
2024-05-13 11:18:22 +0900

    fix : /word/new 요청 조건 추가

--- src/word/dto/request/newWord.request.dto.ts
+++ src/word/dto/request/newWord.request.dto.ts
@@ -1,15 +1,19 @@
 import { User } from "@prisma/client";
 import { Expose } from "class-transformer";
+import { IsAlpha, IsString } from "class-validator";
 
 export class NewWordRequestDto {
   @Expose({
     name: "word"
   })
+  @IsString()
+  @IsAlpha()
   word: string;
 
   @Expose({
     name: "mean"
   })
+  @IsString()
   mean: string;
 
   user: User;
